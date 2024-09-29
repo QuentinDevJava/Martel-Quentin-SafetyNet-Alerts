042da7ab743cb3bb5ceeb4c4b1cd5245f23a0ec4
QuentinDevJava <168752018+QuentinDevJava@users.noreply.github.com>
2024-09-29 17:58:30 +0200

    Add personalized exception

---
+++ src/main/java/com/openclassroom/safetynet/exceptions/FirestationNotFoundException.java
@@ -0,0 +1,11 @@
+package com.openclassroom.safetynet.exceptions;
+
+public class FirestationNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FirestationNotFoundException(String message) {
+		super(message);
+	}
+
+}
